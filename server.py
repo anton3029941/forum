a7d6cb35997abe6f1fb59d6f1ebd970ae8270317
Anton Ni <122563428+anton3029941@users.noreply.github.com>
2026-09-15 12:50:36 +0500

    update

--- server.py
+++ server.py
@@ -6,12 +6,23 @@ database_name = 'database.sqlite'
 
 app = Flask(__name__)
 
+def connect_db():
+    conn = sqlite3.connect(database_name)
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/')
 def home():
-    return render_template('homepage.html')
+    conn = connect_db()
 
-@app.route('/publish', methods=['POST'])
-def publish():
+    posts = conn.execute('SELECT id, title, content, created_at, author FROM posts').fetchall()
+
+    conn.close()
+    
+    return render_template('homepage.html', posts=posts)
+
+@app.route('/create', methods=['POST'])
+def create():
     title = request.form.get('title')
     content = request.form.get('content')
     now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -19,8 +30,8 @@ def publish():
     with sqlite3.connect(database_name) as conn:
         cursor = conn.cursor()
         cursor.execute(f"""
-            INSERT INTO posts (title, content, created_at, author) VALUES (title, content, {now_str},)
-        """)
+            INSERT INTO posts (title, content, created_at, author) VALUES (title, content, created_at, author)
+        """, (title, content, now_str, "author"))
 
     return 201
 
